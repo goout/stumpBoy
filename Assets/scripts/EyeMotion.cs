649f627f8f471805e0e2e98cc7ab8abe11e8b992
Hleb Taraskin <goout@bk.ru>
2018-05-02 13:01:50 +0300

    eyes follow player

---
+++ Assets/scripts/EyeMotion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeMotion : MonoBehaviour
+{
+    [SerializeField] private GameObject target = null;
+    private Transform thisTransform = null;
+
+    void Awake()
+    {
+        thisTransform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.gameObject.transform.position - thisTransform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        thisTransform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
